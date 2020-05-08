2025f9d7bc49939fcf4ba85b0875a02b91ac66b7
sanya <guptasanya090@gmail.com>
2020-05-08 19:52:37 +0530

    gitpush

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("<h1>hello</h1>");
+})
+
+app.listen(process.env.PORT || 5000,(err)=>{
+    console.log("Server started")
+})
